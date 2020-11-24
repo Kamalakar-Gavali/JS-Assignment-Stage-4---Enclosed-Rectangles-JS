f88458c33f5d22cbf7db7d245930615b3eaa2bd8
kamalakar <kg@gmail.com>
2020-11-24 20:41:06 +0530

    did changes

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,79 @@
 //	height: '96px'
 //}
 
-function updateStructure(rec1,rec2){
+function updateStructure(recA,recB){
 	//write your code
+	if(contains(recA,recB))
+	{
+		const relativeDim=relative(recA,recB);
+		return {...recA,children:[relativeDim]};
+	}
+	else if(contains(recB,recA)){
+		const relativeDim=relative(reacB,recA);
+		return {...recB,children:[relativeDim]};
+	}
+	else{
+		return {...recA};
+	}
+}
+function relative(recA,recB)
+{
+	const recAn=normalize(recA);
+	const reacBn=normalize(recB);
+
+	const res={
+		children:recB.children
+	}
+
+	if(recB.top)
+	{
+		res.top=`${recBn.x1-recAn.x1}px`;
+	}
+	if(recB.left)
+	{
+		res.left=`${recBn.y1-recAn.y1}px`;
+	}
+	if(recB.height)
+	{
+		res.height=recB.height;
+	}
+	if(recB.width)
+	{
+		res.width=recB.width;
+	}
+	if(recB.bottom)
+	{
+		res.bottom=`${recAn.x2-recBn.x2}px`;
+	}
+	if(recB.right)
+	{
+		res.right=`${recAn.y2-recBn.y2}px`;
+	}
+	return res;
 }
 
-module.exports = updateStructure;
+function contains(recA,recB)
+{
+	const recAn=normalize(recA);
+	const recBn=normalize(recB);
+	if(recAn.x1<=recBn.x1 && recAn.y1<=recBn.y1 && recAn.x2>=recBn.x2 && recAn.y2>=recBn.y2)
+	{
+		return true;
+	}
+	return false;
+}
+const T=100000;
+const W=100000;
+function normalize(rec)
+{
+	return {
+		x1:rec.top?parseInt(rec.top):(T-(parseInt(rec.bottom)+parseInt(rec.height))),
+		y1:rec.left?parseInt(rec.left):(W-(parseInt(rec.height)+parseInt(rec.width))),
+		x2:rec.bottom?(T-parseInt(rec.bottom)):(parseInt(rec.top)+parseInt(rec.height)),
+		y2:rec.height?(W-parseInt(rec.right)):(parseInt(rec.left)+parseInt(rec.width))
+	}
+}
+
+
+
+export default updateStructure;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/";
 function generateCodeFromObject(obj){
     //return a code generated string
    }
    
-module.exports=generateCodeFromObject;
+export default generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
